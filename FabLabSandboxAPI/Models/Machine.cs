a369e96accab3bb21245d5126351227690bde0df
Andriy Kasyanenko <fiber301@hotmail.com>
2020-10-21 14:58:22 +0200

    Swagger documentations for MakerSpace ready

--- FabLabSandboxAPI/Models/Machine.cs
+++ FabLabSandboxAPI/Models/Machine.cs
@@ -2,6 +2,7 @@ namespace FabLabSandboxAPI.Models
 {
     public class Machine
     {
+
         public int Id { get; set; }
         public string name { get; set; }
         public string description {get; set;}
