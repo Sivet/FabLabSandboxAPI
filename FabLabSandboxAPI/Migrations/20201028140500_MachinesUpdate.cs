c0e3515ab2d8042082d4183692c33dc5b9ecd689
Sivet <simon@glashoff.dk>
2020-10-28 15:13:39 +0100

    Newest Migration

---
+++ FabLabSandboxAPI/Migrations/20201028140500_MachinesUpdate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FabLabSandboxAPI.Migrations
+{
+    public partial class MachinesUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MachineSerialNumber",
+                table: "Machines",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MachineSerialNumber",
+                table: "Machines");
+        }
+    }
+}
